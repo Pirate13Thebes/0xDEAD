fef4b9f382a68753ca6d5f7b7b5ec7a798a9dd8b
fkangira <f.kangira@alustudent.com>
2026-04-16 11:51:47 +0400

    Committing all files quest_01_first_spell.py, quest_02_naming_ceremony.py, quest_03_treasure_chest.py, quest_04_town_crier.py, quest_05_echoing_cave.py and quest_06_fortune_teller.py Python Quests for 0xDEAD repository

---
+++ quest_03_treasure_chest.py
@@ -0,0 +1,6 @@
+# Python distinguishes between text (strings) and whole numbers (integers).
+# Quotes are used for the string, while the integer stands alone.
+age = 21 
+favorite_food = "Sushi"
+
+print(f"I am {age} years old and my favorite food is {favorite_food}.")
